fe081a97eca4250e709a13bb782c0007c383fd17
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-26 23:10:45 +0600

    Navbar done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            <h1>This is Home Components</h1>
+            
         </div>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,99 @@
+import { useState } from "react"
+import { RiMenu3Fill } from "react-icons/ri"
+import { RxCross2 } from "react-icons/rx"
 import { NavLink } from "react-router"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Nav = () => {
+    AOS.init();
+    const [open, setOpen] = useState(false);
+
+
+    const navItem = <>
+    
+        <NavLink 
+        onClick={() => setOpen(false)} 
+        className={({isActive}) => isActive ? `text-yellow-500 font-bold` : `font-semibold hover:text-yellow-500 transition`} to='/'>HOME
+        </NavLink>
+
+        <NavLink 
+        onClick={() => setOpen(false)} 
+        className={({isActive}) => isActive ? `text-yellow-500 font-bold` : `font-semibold hover:text-yellow-500 transition`} 
+        to="/contact-us">
+            CONTACT US
+        </NavLink>
+
+        <NavLink 
+        onClick={() => setOpen(false)} 
+        className={({isActive}) => isActive ? `text-yellow-500 font-bold` : `font-semibold hover:text-yellow-500 transition`} 
+        to="/dashboard">
+            DASHBOARD
+        </NavLink>
+
+        <NavLink 
+        onClick={() => setOpen(false)} 
+        className={({isActive}) => isActive ? `text-yellow-500 font-bold` : `font-semibold hover:text-yellow-500 transition`} 
+        to="/our-menu">
+            OUR MENU
+        </NavLink>
+
+        <NavLink 
+        onClick={() => setOpen(false)} 
+        className={({isActive}) => isActive ? `text-yellow-500 font-bold` : `font-semibold hover:text-yellow-500 transition`} 
+        to="/our-shop">
+            OUR SHOP
+        </NavLink>
+
+        <NavLink 
+        onClick={() => setOpen(false)} 
+        className={({isActive}) => isActive ? `text-yellow-500 font-bold` : `font-semibold hover:text-yellow-500 transition`} 
+        to="/log-in">
+            LOG IN
+        </NavLink>
+    
+    </>
+
     return (
         <header>
             <div className="max-w-[90%] xl:max-w-[1200px] mx-auto flex justify-between items-center py-5">
                 <div>
-                    <h1>
-                        <span>BISTRO BOSS</span>
-                        <span>Restaurant</span>
+                    <h1 className="flex flex-col font-Cinzel text-center font-bold">
+                        <span className="text-xl">BISTROBOSS</span>
+                        <span className="tracking-widest">Restaurant</span>
                     </h1>
                 </div>
 
                 <div>
                     <nav>
-                        <ul className="flex gap-5">
-                            <NavLink to='/'>HOME</NavLink>
-                            <NavLink to="/contact-us">CONTACT US</NavLink>
-                            <NavLink>DASHBOARD</NavLink>
-                            <NavLink>OUR MENU</NavLink>
-                            <NavLink>OUR SHOP</NavLink>
-                            <NavLink>LOG OUT</NavLink>
+                        <ul className="lg:flex hidden gap-5">
+                            {navItem}
                         </ul>
                     </nav>
                 </div>
+
+                <button onClick={() => setOpen(!open)} className="text-3xl font-bold lg:hidden">
+
+                    {
+                        open ? <RxCross2 /> : <RiMenu3Fill />
+                    }
+                    
+                </button>
+
             </div>
+                {
+                    open ? 
+                    <div>
+                        <nav className="lg:hidden relative">
+                            <ul 
+                            data-aos="fade-left" 
+                            className="flex flex-col gap-5 absolute -top-4 py-5 px-20 right-2 bg-gray-500 text-center border border-gray-200 rounded-xl transition delay-75">
+                                {navItem}
+                            </ul>
+                        </nav>
+                    </div> 
+                    : ''
+                }
         </header>
     )
 }

28f57130f2479a0f8e861ad696019cf8d78a1d78
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-27 21:17:39 +0600

    Bistro Updated

--- src/components/Bistro.jsx
+++ src/components/Bistro.jsx
@@ -2,8 +2,8 @@ import './compo.css'
 
 const Bistro = () => {
     return (
-        <section className="p-16 mb-16 max-w-[90%] xl:max-w-[900px] mx-auto bistro rounded-lg">
-            <div className='text-center bg-white text-black p-10 rounded-xl'>
+        <section className="sm:p-16 p-8 mb-16 max-w-[90%] xl:max-w-[900px] mx-auto bistro rounded-lg">
+            <div className='text-center bg-white text-black p-3 sm:p-10 rounded-xl'>
                 <h1 className='text-3xl font-Cinzel font-bold mb-3'>Bistro Boss</h1>
                 <p>In the digital age, restaurants are not just about food; they are about experiences, convenience, and connection. Bistro Boss, a dynamic restaurant website, epitomizes this modern approach, seamlessly blending technology with the art of gastronomy. Whether you’re a food enthusiast, a busy professional, or someone planning a memorable dining experience, Bistro Boss has something for everyone.</p>
             </div>
